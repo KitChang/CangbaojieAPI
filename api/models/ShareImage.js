dc897d28f7a813137bcc23d63969d575e28ca02e
KitChang <rdjk287@gmail.com>
2016-01-28 16:39:16 +0800

    add share info

---
+++ api/models/ShareImage.js
@@ -0,0 +1,15 @@
+/**
+* ShareImage.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  identity: 'ShareImage',
+  attributes: {
+      replaced: {type: 'boolean', defaultsTo: false},
+      advertisement: {model: 'advertisement'}
+  }};
+
